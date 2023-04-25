b9b2347f60109bdbb4315afed8cbfa73b5171f49
stephanniexz <82967399+stephanniexz@users.noreply.github.com>
2023-04-26 01:17:48 +0800

    setup

--- recommender/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'bootstrap/dist/js/bootstrap.min.js'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
